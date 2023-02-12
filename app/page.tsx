3dc149cc6765f30a8213b52094d4a0841e8562e6
SyntaxSage <--g>
2023-02-12 15:12:45 +0200

    tailwind ready

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return <>
     <main>
       <b>
-      Hello World
+       <h1 className="bg-slate-500 color-white text-white text-6xl">Hello World</h1>
       </b>
     </main>
   </>;
